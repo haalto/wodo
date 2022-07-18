a0fb09232cb1fd5d6b3b8ccdd35183519035cc28
Hannes Aaltonen <aaltonen.hannes@gmail.com>
2022-07-18 18:33:58 +0300

    foo

--- app/lib/dynamodb.ts
+++ app/lib/dynamodb.ts
@@ -3,10 +3,10 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDB({
   endpoint: process.env.DYNAMODB_URL,
-  region: process.env.AWS_REGION || "eu-north-1",
+  region: process.env.REGION || "eu-north-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    accessKeyId: process.env.ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.SECRET_ACCESS_KEY || "",
   },
 });
 
